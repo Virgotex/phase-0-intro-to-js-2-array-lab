23281518b3192759ff58e4a0472eee7957a38b13
Carlton Kibe <Virgotex15@gmail.com>
2023-03-26 16:01:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+destructivelyAppendCat('Ralph')
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+destructivelyPrependCat('Bob')
+
+function destructivelyRemoveLastCat(){
+    let destructivelyRemoveLastCat = cats.pop()
+    return destructivelyRemoveLastCat
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat(){
+    let destructivelyRemoveFirstCat = cats.shift()
+    return destructivelyRemoveFirstCat
+}
+destructivelyRemoveFirstCat()
+
+function appendCat(name){
+    const catArr = [...cats, name]
+    return catArr
+}
+appendCat('Broom')
+
+function prependCat(name){
+    const catArr = [name, ...cats]
+    return catArr
+}
+prependCat('Arnold')
+
+function removeFirstCat(){
+    const firstItem = cats.slice(1)
+    return firstItem
+}
+removeFirstCat()
+
+function removeLastCat(){
+    const lastItem = cats.slice(0,2)
+    return lastItem
+}
+removeLastCat()
